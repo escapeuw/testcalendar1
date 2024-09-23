05d77300a5e78d62f7f38a6d1d33b57c23ef0b9d
escapeuw <ghkdehd78999@gmail.com>
2024-09-22 22:45:34 -0400

    testingUI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,147 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Calendar, { DecadeView } from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+import './App.css'
+import { getValue } from '@testing-library/user-event/dist/utils';
+const mykey = 'AIzaSyCOXyQm3a7XEJ6Rm3nrWcmACARux9O-qkQ'
+export default function CalendarGfg() {
+    const [value, onChange] = useState(new Date());
+    var now = new Date();
+    /* getting next month date info
+    var current = new Date(now.getFullYear(), now.getMonth()+1, 1); */
+    return (
+        <div>
+            <h1>Test</h1>
+            <Calendar
+                value={value}
+                locale="en-US"
+                minDetail='month'
+                prev2Label={null}
+                prevLabel={null}
+                nextLabel={null}
+                next2Label={null}
+            />
+            <Calendar
+                onChange={onChange}
+                value={new Date(now.getFullYear(), now.getMonth()+1, 1)}
+                locale="en-GB"
+                minDetail='month'
+                prev2Label={null}
+                prevLabel={null}
+                nextLabel={null}
+                next2Label={null}
+            />
+            <Calendar
+                onChange={onChange}
+                value={new Date(now.getFullYear(), now.getMonth()+2, 1)}
+                locale="en-GB"
+                minDetail='month'
+                prev2Label={null}
+                prevLabel={null}
+                nextLabel={null}
+                next2Label={null}
+            />
+            <Calendar
+                onChange={onChange}
+                value={new Date(now.getFullYear(), now.getMonth()+3, 1)}
+                locale="en-GB"
+                minDetail='month'
+                prev2Label={null}
+                prevLabel={null}
+                nextLabel={null}
+                next2Label={null}
+            />
+            <Calendar
+                onChange={onChange}
+                value={new Date(now.getFullYear(), now.getMonth()+4, 1)}
+                locale="en-GB"
+                minDetail='month'
+                prev2Label={null}
+                prevLabel={null}
+                nextLabel={null}
+                next2Label={null}
+            />
+            <Calendar
+                onChange={onChange}
+                value={new Date(now.getFullYear(), now.getMonth()+5, 1)}
+                locale="en-GB"
+                minDetail='month'
+                prev2Label={null}
+                prevLabel={null}
+                nextLabel={null}
+                next2Label={null}
+            />
+            <Calendar
+                onChange={onChange}
+                value={new Date(now.getFullYear(), now.getMonth()+6, 1)}
+                locale="en-GB"
+                minDetail='month'
+                prev2Label={null}
+                prevLabel={null}
+                nextLabel={null}
+                next2Label={null}
+            />
+            <Calendar
+                onChange={onChange}
+                value={new Date(now.getFullYear(), now.getMonth()+7, 1)}
+                locale="en-GB"
+                minDetail='month'
+                prev2Label={null}
+                prevLabel={null}
+                nextLabel={null}
+                next2Label={null}
+            />
+            <Calendar
+                onChange={onChange}
+                value={new Date(now.getFullYear(), now.getMonth()+8, 1)}
+                locale="en-GB"
+                minDetail='month'
+                prev2Label={null}
+                prevLabel={null}
+                nextLabel={null}
+                next2Label={null}
+            />
+            <Calendar
+                onChange={onChange}
+                value={new Date(now.getFullYear(), now.getMonth()+9, 1)}
+                locale="en-GB"
+                minDetail='month'
+                prev2Label={null}
+                prevLabel={null}
+                nextLabel={null}
+                next2Label={null}
+            />
+            <Calendar
+                onChange={onChange}
+                value={new Date(now.getFullYear(), now.getMonth()+10, 1)}
+                locale="en-GB"
+                minDetail='month'
+                prev2Label={null}
+                prevLabel={null}
+                nextLabel={null}
+                next2Label={null}
+            />
+            <Calendar
+                onChange={onChange}
+                value={new Date(now.getFullYear(), now.getMonth()+11, 1)}
+                locale="en-GB"
+                minDetail='month'
+                prev2Label={null}
+                prevLabel={null}
+                nextLabel={null}
+                next2Label={null}
+            />
+            <Calendar
+                onChange={onChange}
+                value={new Date(now.getFullYear(), now.getMonth()+12, 1)}
+                locale="en-GB"minDetail='month'
+                prev2Label={null}
+                prevLabel={null}
+                nextLabel={null}
+                next2Label={null}
+            />
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        </div>
+    );
 }
 
-export default App;
